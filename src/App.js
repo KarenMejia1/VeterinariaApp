faebf34b38c8c72a4e587672685191db6689fdd1
Karen Mejia <karen.mejia@softtek.com>
2022-02-16 18:41:59 -0600

    Veterinaria terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+import { Formulario } from './components/Formulario';
+import Header from './components/Header';
+import { ListadoPacientes } from './components/ListadoPacientes';
+
+
+export const App = () => {
+
+  const [pacientes, setPacientes] = useState([]);
+
+  // Para editar
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    //Obtener lo que haya en el localStorage
+    const obtenerLs = () => {
+      const pacientesLs = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+
+      setPacientes(pacientesLs)
+    }
+
+    obtenerLs();
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
+
+  const eliminarPaciente = (id) => {
+
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container mx-auto mt-20'>
+
+      <Header
+      />
+
+      <div className='mt-12 md:flex'>
+
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+
+      </div>
+
     </div>
   );
 }
-
-export default App;
